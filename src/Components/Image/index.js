e2563c4e67dee8f4cf6191f388a745f1663b0aa6
Walter Shewmake <waltershewmake@gmail.com>
2023-03-15 02:56:58 +0000

    removed uneeded console logs

--- src/Components/Image/index.js
+++ src/Components/Image/index.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import './style.css';
+import React from "react";
+import "./style.css";
 
-const Image = ({ before,after, alt, height ,onLoad , onError }) => {
+const Image = ({ before, after, alt, height, onLoad, onError }) => {
+	return (
+		<div>
+			<img
+				name="after"
+				onError={onError}
+				onLoad={onLoad}
+				src={after}
+				id="_image"
+				style={{ height }}
+				alt={alt ?? "after"}
+			/>
 
-    return (
-    
-    <div>
-
-
-        <img  name="after"  onError={onError} onLoad={onLoad}  src={after} id="_image" style={{ height }} alt={alt} />
-
-        <img name="before" onError={onError} onLoad={onLoad} src={before} id="_image_after" style={{ height:"0px" }} alt={alt} />
-
-
-    </div>
-    
-    
-    );
-}
+			<img
+				name="before"
+				onError={onError}
+				onLoad={onLoad}
+				src={before}
+				id="_image_after"
+				style={{ height: "0px" }}
+				alt={alt ?? "before"}
+			/>
+		</div>
+	);
+};
 
 export default Image;
